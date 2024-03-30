bca1a215c4c1037d79dbc9b78460b8534f0047bb
xacademy_norbi <norbi.xacademy@gmail.com>
2024-03-30 22:46:50 +0100

    Small modifications

---
+++ src/conf/mysql.ts
@@ -0,0 +1,9 @@
+import { MysqlConfigOptions } from "../types/mysql-config.js";
+
+export const mysqlConfig: MysqlConfigOptions = {
+	host: "localhost",
+	port: 3306,
+	username: "root",
+	password: "1234",
+	database: "vatnotif",
+};
---
+++ src/conf/push.ts
@@ -0,0 +1,6 @@
+import { VAPIDKeysOptions } from "../types/vapid-keys.js";
+
+export const VAPIDKeys: VAPIDKeysOptions = {
+	publicKey: "1234",
+	privateKey: "1234",
+};
